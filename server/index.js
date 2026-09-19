fbc85e2cbbfe5b9c49f5846fa9a152fc07391b7c
Raunak <2300520400051@ietlucknow.ac.in>
2026-09-19 08:54:22 +0530

    Change import statement for cors to require syntax
    cors syntax changed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-import cors from "cors";
+const cors = require("cors");
 const pdfParse = require("pdf-parse");
 const fs = require("fs");
 const { GoogleGenAI } = require('@google/genai');
